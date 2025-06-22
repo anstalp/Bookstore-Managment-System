9272d64fa01d507173288834be339fce55ee6d4e
anstalp <aalpanidis@gmail.com>
2025-06-23 00:55:39 +0300

    v3.3

--- src/main/java/com/bookstore/bookstore/security/SecurityConfig.java
+++ src/main/java/com/bookstore/bookstore/security/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://bookstore-managment-system.netlify.app"));
+        configuration.setAllowedOriginPatterns(List.of("https://bookstore-managment-system.netlify.app"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
